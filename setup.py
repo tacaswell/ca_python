2ab990441d2910bc94933c727151b0e17facac1e
friedrich-schotte <friedrich.schotte@gmail.com>
2017-04-01 19:35:27 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ca_python',
+  packages = ['ca_python'], # this must be the same as the name above
+  version = '2.0.3',
+  description = 'EPICS Channel Access Protocol',
+  author = 'Friedrich',
+  author_email = 'friedrich.schotte@gmail.com',
+  url = 'https://github.com/friedrich-schotte/ca_python',
+  download_url = 'https://github.com/friedrich-schotte/ca_python/archive/2.0.3.tar.gz', 
+  keywords = ['distributed instrumentation'], 
+  classifiers = [],
+)
