7b7b2c53d85322959ef13b30eb7fcd6b88541b37
friedrich-schotte <friedrich.schotte@gmail.com>
2017-04-02 01:28:38 -0400

    Changed PyPI version name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'EPICS-CA',
   packages = ['EPICS_CA'], # this must be the same as the name above
-  version = '2.0.3.1',
+  version = '2.0.3.2',
   description = 'EPICS Channel Access Protocol',
   author = 'Friedrich',
   author_email = 'friedrich.schotte@gmail.com',
