3e8aea0d2c2f314ac4cf8f3b197fbeff08dfda0b
friedrich-schotte <friedrich.schotte@gmail.com>
2017-04-02 00:52:44 -0400

    Changed directory structure

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'EPICS-CA',
-  packages = ['EPICS-CA'], # this must be the same as the name above
-  version = '2.0.3',
+  packages = ['EPICS_CA'], # this must be the same as the name above
+  version = '2.0.3.1',
   description = 'EPICS Channel Access Protocol',
   author = 'Friedrich',
   author_email = 'friedrich.schotte@gmail.com',
   url = 'https://github.com/friedrich-schotte/ca_python',
-  download_url = 'https://github.com/friedrich-schotte/ca_python/archive/2.0.3.tar.gz', 
+  download_url = 'https://github.com/friedrich-schotte/ca_python/archive/2.0.3.tar.gz',
   keywords = ['distributed instrumentation'], 
   classifiers = [],
 )
