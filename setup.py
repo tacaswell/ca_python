2610b4c81414cef89bae6b11c13cdcf525162d5a
friedrich-schotte <friedrich.schotte@gmail.com>
2017-04-02 02:17:15 -0400

    Changed directory structrue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'EPICS-CA',
   packages = ['EPICS_CA'], 
-  version = '2.0.3.5',
+  version = '2.0.3.6',
   description = 'EPICS Channel Access Protocol',
   author = 'Friedrich',
   author_email = 'friedrich.schotte@gmail.com',
